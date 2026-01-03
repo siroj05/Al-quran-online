1ea7be09d2bf8a07c1e269a975d78e37f696bf81
siroj05 <rojudin123@gmail.com>
2026-01-03 23:02:35 +0700

    add home redirect on logo button

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -21,10 +21,10 @@ watch(search, (value) => {
 <template>
   <nav class="py-3 text-white w-full bg-green-800 top-0 z-50 sticky">
     <div class="flex mx-auto max-w-5xl">
-      <div class="flex">
+      <button @click="router.push({ name: 'home' })" class="ml-3 cursor-pointer flex">
         <img src="./icons/alquran logo.png" alt="Al-Qur'an" class="w-12 h-12" />
         <h1 class="font-bold text-2xl my-auto">Al-Qur'an</h1>
-      </div>
+      </button>
       <!-- <div class="mx-auto my-auto">
         <input type="text" class="bg-white text-black px-2 rounded-lg w-100 h-9" v-model="search" />
       </div> -->
